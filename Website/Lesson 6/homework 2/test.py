acc15663c7c1166d99e3f1df83887c90d5002ba7
Python <abdumavlonovabdurahmon75@gmail.com>
2024-04-17 09:52:00 +0500

    Website lesson 1-s

---
+++ Website/Lesson 6/homework 2/test.py
@@ -0,0 +1,43 @@
+
+# def func(arr):
+#     a = arr[1]
+#     b = arr[3]
+#     c = arr[5]
+#     perimetr = a + b + c
+#     yuza = (a + b) / 2
+#     print(perimetr)
+#     print(yuza)
+
+# func([1,3,6,5,8,4,7,5])
+
+
+# name = input("Ismingizni kiriting! ")
+# lastname = input("Familangizni kiriting! ")
+# girl = ["Maftuna", "Nargiza","Malika"]
+# boy = ["Anvar", "Bobur","Ali","Umar"]
+# if lastname.endswith("a"):
+#     if name in girl:
+#         print("Salom",name,"siz ro'yxatda borsiz!")
+#     else:
+#         print("Salom",name,"siz qizlar royxatda yoqsiz! ")
+#         orb = input("Ro'yxatga qoshilmoqchimisiz? ")
+# else:
+#     if name in boy:
+#         print("Salom",name,"siz bolalar royxatda borsiz!")
+#     else:
+#         print("Salom",name,"siz bolalar royxatida yoqsiz")
+#         bit = input("Ro'yxatga qoshilmoqchimisiz? ")
+
+
+# son = int(input("Sonni kiriting! "))
+# if son % 2 == 0:
+#     print(son,"bu son juft")
+# else:
+#     print(son,"bu son toq son")
+
+
+
+
+oylik = float(input("Oylikgizgizni kiriting! "))
+sof_oylik = oylik * 0,88
+print("sizni sof oyligiz")
